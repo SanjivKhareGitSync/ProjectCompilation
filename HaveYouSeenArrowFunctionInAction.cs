fe715969733ff09bad5236187e95695017c723e7
SanjivKhareGitSync <sanjivjazz@gmail.com>
2026-02-06 17:24:10 +0530

    Rearrange files from a directory in folders based on file type.

---
+++ HaveYouSeenArrowFunctionInAction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCompilation
+{
+    internal static class HaveYouSeenArrowFunctionInAction
+    {
+        public static bool IsDocument(this string path) => FileCategorySets.Documents.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsImage(this string path) => FileCategorySets.Images.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsZipRar(this string path) => FileCategorySets.zipRar.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsAnyAllowed(this string path) => FileCategorySets.AllAllowed.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsGroupCatFile(this string path) => FileCategorySets.GroupCatServiceFiles.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsVideo(this string path) => FileCategorySets.Video.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsExcelFile(this string path) => FileCategorySets.ExcelFile.Contains(Path.GetExtension(path).ToLower());
+        public static bool IsHot(this int number) => number == 69;
+    }
+
+    public static class FileCategorySets
+    {
+        public static readonly HashSet<string> GroupCatServiceFiles = new() {
+            ".GMUVS",".sql"
+        };
+
+        public static readonly HashSet<string> ExcelFile = new() {
+            ".xls", ".xlsx"
+        };
+
+        public static readonly HashSet<string> Documents = new() {
+            ".html", ".txt", ".docx", ".pdf", ".xls", ".xlsx"
+        };
+
+        public static readonly HashSet<string> DataFiles = new() {
+            ".csv"
+        };
+
+        public static readonly HashSet<string> Scripts = new() {
+            ".py"
+        };
+
+        public static readonly HashSet<string> Images = new() {
+            ".png", ".jpg", ".jpeg", ".gif"
+        };
+
+        public static readonly HashSet<string> Video = new() {
+            ".mp4"
+        };
+
+        public static readonly HashSet<string> zipRar = new() {
+            ".zip", ".rar"
+        };
+
+        public static readonly HashSet<string> Reports = new() {
+            ".trdx"
+        };
+
+        // All combined (for "any" check)
+        public static readonly HashSet<string> AllAllowed = new() {
+            ".html", ".py", ".csv", ".txt", ".docx", ".pdf",
+            ".xls", ".xlsx", ".png", ".jpg", ".jpeg", ".gif",
+            ".mp4", ".zip", ".rar", ".trdx"
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-
-
+﻿using ProjectCompilation;
+using System;
 
 
 namespace TheImpracticalNameSpace
@@ -9,8 +8,15 @@ namespace TheImpracticalNameSpace
     {
         private static async Task Main(string[] args)
         {
-            AsyncSyncTaskThredAllInOne asyncSyncTaskThredAllInOne = new AsyncSyncTaskThredAllInOne();
-            await asyncSyncTaskThredAllInOne.Run();
+            //AsyncSyncTaskThredAllInOne asyncSyncTaskThredAllInOne = new AsyncSyncTaskThredAllInOne();
+            //await asyncSyncTaskThredAllInOne.Run();
+
+            IOStreamDream iOStreamDream = new IOStreamDream();
+            string path = "E:\\desktop";
+            iOStreamDream.DestinationDirectory = "E:\\GCFilesNew";
+            //iOStreamDream.Run("E:\\all downloads\\old downloads\\pandas numpy django\\pandas numpy django", 4);
+            iOStreamDream.Run(path, 7);
+            //iOStreamDream.RunCollectFileNames("E:\\d");
         }
     }
 }
